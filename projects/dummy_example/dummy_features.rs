d16ff4ee0e0d1341222132505214026d1255a7d0
Janne Kauppinen <janne.a.kauppinen@student.jyu.fi>
2022-11-27 11:32:56 +0200

    DummyExample features.

---
+++ projects/dummy_example/dummy_features.rs
@@ -0,0 +1,56 @@
+use ufo3000::template::{
+    WGPUFeatures,
+};
+
+/// Features and limits for DummyExample application.
+pub struct DummyExampleFeatures {}
+
+impl WGPUFeatures for DummyExampleFeatures {
+
+    fn optional_features() -> wgpu::Features {
+        wgpu::Features::TIMESTAMP_QUERY
+    }
+
+    fn required_features() -> wgpu::Features {
+
+        // #[cfg(not(target_arch = "wasm32"))] {
+        if cfg!(not(target_arch = "wasm32")) {
+            wgpu::Features::PUSH_CONSTANTS |
+            wgpu::Features::WRITE_TIMESTAMP_INSIDE_PASSES
+        }
+        else {
+        // #[cfg(target_arch = "wasm32"))] {
+            wgpu::Features::empty()
+        }
+    }
+
+    fn required_limits() -> wgpu::Limits {
+        let mut limits = wgpu::Limits::default();
+
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+        limits.max_compute_invocations_per_workgroup = 1024;
+        }
+
+        #[cfg(not(target_arch = "wasm32"))] 
+        {
+        limits.max_compute_workgroup_size_x = 1024;
+        }
+
+        limits.max_storage_buffers_per_shader_stage = 10;
+
+        #[cfg(not(target_arch = "wasm32"))] 
+        {
+        limits.max_push_constant_size = 4; // Now useless.
+        }
+
+        #[cfg(not(target_arch = "wasm32"))] 
+        {
+        limits.max_push_constant_size = 4; // Now useless.
+        }
+
+        limits.max_storage_buffer_binding_size = 396441600; 
+
+        limits
+    }
+}
--- projects/dummy_example/main.rs
+++ projects/dummy_example/main.rs
@@ -1,5 +1,7 @@
-use ufo3000::logger::initialize_simple_logger;
+// use crate::features::*;
+mod dummy_features;
 use log::LevelFilter;
+use ufo3000::logger::initialize_simple_logger;
 use ufo3000::input::*;
 use ufo3000::template::{
     WGPUFeatures,
@@ -9,9 +11,11 @@ use ufo3000::template::{
     Spawner,
 };
 
+
 fn main() {
 
     initialize_simple_logger(&vec![("dummy_example".to_string(), LevelFilter::Info)]);
+    let dummy_features = dummy_features::DummyExampleFeatures {};
 
     log::info!("Hekotus from dummy_example.");
 }
