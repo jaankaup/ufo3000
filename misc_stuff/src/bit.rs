91c66c62a88adec78554340a6094e1e5f1312efc
Janne Kauppinen <janne.a.kauppinen@student.jyu.fi>
2022-12-02 10:37:43 +0200

    Some tests.

--- misc_stuff/src/bit.rs
+++ misc_stuff/src/bit.rs
@@ -1,11 +1,14 @@
+#[inline]
 pub fn zero_bit(n: u32, i: u32) -> u32 {
    n & (!(1 << i)) 
 }
 
+#[inline]
 pub fn one_bit(n: u32, i: u32) -> u32 {
-   n | (!(1 << i)) 
+   n | (1 << i) 
 }
 
+#[inline]
 pub fn swap_bit(n: u32, i: u32) -> u32 {
     n ^ (1 << i)
 }
--- misc_stuff/src/lib.rs
+++ misc_stuff/src/lib.rs
@@ -4,13 +4,41 @@ pub mod bit;
 #[cfg(test)]
 
 mod tests {
-use crate::bit::zero_bit;
+use crate::bit::{zero_bit, one_bit, swap_bit};
+
     #[test]
-    fn zero_bits_test() {
+    fn zero_bits_test_0() {
         let result = zero_bit(0b00000010001100000000000010000000, 7);
         assert_eq!(result, 0b00000010001100000000000000000000);
+    }
 
+    #[test]
+    fn zero_bits_test_1() {
         let result = zero_bit(0b00000010001100000000000010000000, 8);
         assert_eq!(result, 0b00000010001100000000000010000000);
+    }
+
+    #[test]
+    fn one_bits_test_0() {
+        let result = one_bit(0b00000010001100000000000010000000, 31);
+        assert_eq!(result, 0b10000010001100000000000010000000);
+    }
+
+    #[test]
+    fn one_bits_test_1() {
+        let result = one_bit(0b00000010001100000000000010000001, 0);
+        assert_eq!(result, 0b00000010001100000000000010000001);
+    }
+
+    #[test]
+    fn swap_bits_test_0() {
+        let result = swap_bit(0b00000010001100000000000010000000, 2);
+        assert_eq!(result, 0b00000010001100000000000010000100);
+    }
+
+    #[test]
+    fn swap_bits_test_1() {
+        let result = swap_bit(0b00000010001100000000000010000000, 7);
+        assert_eq!(result, 0b00000010001100000000000000000000);
     }
 }
